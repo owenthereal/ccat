1453bf67e04d198e3186b9654198066b865a7111
Jingwen Owen Ou <jingweno@gmail.com>
2015-05-27 17:20:43 -0700

    Switch to use github.com/spf13/cobra for flag parsing

--- main.go
+++ main.go
@@ -2,84 +2,60 @@ package main
 
 import (
 	"log"
-	"os"
 
-	"github.com/jingweno/ccat/Godeps/_workspace/src/github.com/codegangsta/cli"
 	"github.com/jingweno/ccat/Godeps/_workspace/src/github.com/mattn/go-colorable"
+	"github.com/jingweno/ccat/Godeps/_workspace/src/github.com/spf13/cobra"
 )
 
 const (
 	readFromStdin = "-"
 )
 
-func init() {
-	cli.AppHelpTemplate = `NAME:
-    {{.Name}} - {{.Usage}}
-
-USAGE:
-    {{.Name}} [options] [file ...]
-
-VERSION:
-    {{.Version}}
-
-OPTIONS:
-	{{range .Flags}}{{.}}
-	{{end}}
-Using color is auto both by default and with --color=auto. With --color=auto,
-ccat emits color codes only when standard output is connected to a terminal.
-`
+type ccatCmd struct {
+	BGFlag    string
+	ColorFlag string
 }
 
-func main() {
-	app := cli.NewApp()
-	app.Name = "ccat"
-	app.Usage = "Colorize FILE(s), or standard input, to standard output."
-	app.Version = Version
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "bg",
-			Value: "light",
-			Usage: `Set to "light" or "dark" depending on the terminal's background.`,
-		},
-		cli.StringFlag{
-			Name:  "C, color",
-			Value: "auto",
-			Usage: `Colorize the output; Value can be "never", "always" or "auto".`,
-		},
-	}
-	app.Action = runCCat
-
-	app.Run(os.Args)
-}
-
-func runCCat(c *cli.Context) {
+func (c *ccatCmd) Run(cmd *cobra.Command, args []string) {
 	var colorDefs ColorDefs
-	if c.String("bg") == "dark" {
+	if c.BGFlag == "dark" {
 		colorDefs = DarkColorDefs
 	} else {
 		colorDefs = LightColorDefs
 	}
 
 	var printer CCatPrinter
-	if c.String("color") == "always" {
+	if c.ColorFlag == "always" {
 		printer = ColorPrinter{colorDefs}
-	} else if c.String("color") == "never" {
+	} else if c.ColorFlag == "never" {
 		printer = PlainTextPrinter{}
 	} else {
 		printer = AutoColorPrinter{colorDefs}
 	}
 
-	fnames := c.Args()
 	// if there's no args, read from stdin
-	if len(fnames) == 0 {
-		fnames = []string{readFromStdin}
+	if len(args) == 0 {
+		args = []string{readFromStdin}
 	}
 
 	stdout := colorable.NewColorableStdout()
-	for _, fname := range fnames {
-		err := CCat(fname, printer, stdout)
+	for _, arg := range args {
+		err := CCat(arg, printer, stdout)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func main() {
+	ccatCmd := &ccatCmd{}
+	rootCmd := &cobra.Command{
+		Use:  "ccat [file ...]",
+		Long: "Colorize FILE(s), or standard input, to standard output.",
+		Run:  ccatCmd.Run,
+	}
+
+	rootCmd.PersistentFlags().StringVarP(&ccatCmd.BGFlag, "bg", "", "light", `Set to "light" or "dark" depending on the terminal's background`)
+	rootCmd.PersistentFlags().StringVarP(&ccatCmd.ColorFlag, "color", "C", "auto", `colorize the output; value can be "never", "always" or "auto"`)
+	rootCmd.Execute()
 }
